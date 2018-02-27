90a5c2c15b3c6e2815f2bfa1a2988bd603f04dfc
Rajkumar M S (BAS) <RA392826@wipro.com>
2018-02-27 19:12:25 +0530

    final commit6

--- src/test/java/com/wipro/mavenpro/AssignmentNew1/TC01_RegistrationandAddtoCart.java
+++ src/test/java/com/wipro/mavenpro/AssignmentNew1/TC01_RegistrationandAddtoCart.java
@@ -37,6 +37,7 @@ public class  TC01_RegistrationandAddtoCart extends InitiateDriver {
 		//adding one line
 		String num;
 		String num2;
+		//new line 2
 		
 		//Fill data in Create Account page
 		AccountRegister.FirstName(driver).sendKeys(hashdata.get("FirstName"));
